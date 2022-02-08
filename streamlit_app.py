e589d6b3015bfb3049d72d26fe1d3d52878cf99c
ShirleyWang7 <99060105+ShirleyWang7@users.noreply.github.com>
2022-02-08 18:26:46 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,12 @@ import math
 import pandas as pd
 import streamlit as st
 import numpy as np
+from PIL import Image
 
+
+st.title("APP Title")
+
+st.header("APP Name Undecided")
 
 
 df1 = pd.DataFrame(
@@ -12,5 +17,48 @@ df1 = pd.DataFrame(
 
 st.write(df1)
 
+st.table(df1)
+
 st.line_chart(df1)
+
+st.map(df1)
+
+input1 = st.button("Please enter your name")
+
+choice1 = st.radio("What is your age?",("under 18","18 to 29", "30 to 40", "above 40"))
+
+image = Image.open('sunrise.jpg')
+st.image(image, caption = 'figure 1')
+
+col1,col2 = st.columns(2)
+
+with st.container():
+    
+    st.write("Within Container")
+    
+    st.map(df1)
+    
+    st.write("Outside Container")
+    
+ 
+
+gender = st.text_input('Gender')
+if not gender:
+  st.warning('Please enter a valid gender')
+  st.stop()
+st.success('Thanks!')
+
+st.help(df1)
+
+st.experimental_show(df1)
+
+df2 = pd.DataFrame(
+    np.random.randint(100,500,(5,5)),
+    columns=['Col 1','Col 2','Col 3','Col 4','Col 5'])
+
+table1 = st.table(df1)
+table1.add_rows(df2)
+
+
+
 
